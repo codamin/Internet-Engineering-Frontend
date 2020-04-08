63bf75b32c424163a4d3edbc6c8537e8ed471cb0
Hossein Entezari <h.entezari78@gmail.com>
2020-04-08 07:10:07 -0700

    order item style improve

--- src/components/restaurant/restaurantContainer/cart/orderItem.js
+++ src/components/restaurant/restaurantContainer/cart/orderItem.js
@@ -1,27 +1,27 @@
 import React from 'react'
 
-import './orderItem'
+import './orderItem.css'
 
 function OrderItem(props) {
     if(!props || props.item == undefined){
         return null; //You can change here to put a customized loading spinner 
     }
     return(
-        <div>
+        <div className="order-item-card">
             <div className="row mt-2 no-gutters">
                 <a href="#" className="col-1 flaticon-minus minus-btn no-gutters justify-content-start"></a>
                 <div className="col-2 flex-container no-gutters justify-content-center food-num-cart">
                     {props.item.number}
                 </div>
                 <a href="#" className="col-1 flaticon-plus plus-btn no-gutters justify-content-end"></a>
-                <div className="col-8 flex-container justify-content-end food-name-cart">
+                <div className="col-8 flex-container justify-content-end food-name-cart persian text-right">
                     {props.item.food.name}
                 </div>
             </div>
             <div className="row pt-0 pr-0 pb-2 pl-0 food-field-bottom container no-gutters">
                 <div className="col-3 pr-1 flex-container justify-content-end">تومان</div>
                 <div className="col-3 flex-container justify-content-start">
-
+                {props.item.price}
                 </div>
                 <div className="col-6"></div>
             </div>
