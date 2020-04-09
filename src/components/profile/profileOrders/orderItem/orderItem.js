8f88d30fbd0f0ef0548ba3ef744ddf490faa644a
Amin Asadi <aminasadi329@gmail.com>
2020-04-09 11:29:17 +0430

    rename order item styles

--- src/components/profile/profileOrders/orderItem/orderItem.js
+++ src/components/profile/profileOrders/orderItem/orderItem.js
@@ -3,21 +3,21 @@ import './orderItem.css'
 
 function RenderDone(props) {
     return(
-    <button className="btn stat-button stat-active text-center enabled" data-toggle="modal" data-target={"#order_modal_" + props.id}>
+    <button className="btn profile-stat-button profile-stat-active text-center enabled" data-toggle="modal" data-target={"#order_modal_" + props.id}>
         مشاهده فاکتور
     </button>);
 }
 
 function RenderInWay() {
     return(
-        <button className="btn stat-button stat-in-way text-center disabled">
+        <button className="btn profile-stat-button profile-stat-in-way text-center disabled">
             پیک در مسیر
         </button>);
 }
 
 function RenderFinding() {
     return(
-        <button className="btn stat-button stat-finding text-center disabled">
+        <button className="btn profile-stat-button profile-stat-finding text-center disabled">
             در جستجوی پیک
         </button>);
 }
@@ -42,11 +42,11 @@ function OrderItem(props) {
     return (
         <div>
             <div className="row order-row">
-                <div className="col-5 order-col order-left-col text-center">
+                <div className="col-5 profile-order-col profile-order-left-col text-center">
                     {renderButton(props.orderData.state, props.id)}
                 </div>
-                <div className="col-5 order-col text-center">رستوران خامس</div>
-                <div className="col-2 order-col text-center order-right-col">{props.id + 1}</div>
+                <div className="col-5 profile-order-col text-center">رستوران خامس</div>
+                <div className="col-2 profile-order-col text-center profile-order-right-col">{props.id + 1}</div>
             </div>
             
             <div className="modal fade" id={"order_modal_" + props.id} tabindex="-1" role="dialog" aria-labelledby="mymodalLabel" aria-hidden="true">
@@ -55,11 +55,11 @@ function OrderItem(props) {
                         <div className="modal-body">
                             <div className="container-fluid">
                                 <div className="row justify-content-center modal-title-row">
-                                    <div className="col-md-6 justify-content-center modal-title-col">رستوران خامس</div>
+                                    <div className="col-md-6 justify-content-center profile-modal-title-col">رستوران خامس</div>
                                 </div>
                                 <table className="table table-bordered text-center">
                                     <thead>
-                                        <tr className="modal-table-header">
+                                        <tr className="profile-modal-table-header">
                                             <th scope="col">قیمت</th>
                                             <th scope="col">تعداد</th>
                                             <th scope="col">نام غذا</th>
@@ -73,7 +73,7 @@ function OrderItem(props) {
                                     </tbody>
                                 </table>
                             </div>
-                            <div className="col-auto total-cost-row">
+                            <div className="col-auto profile-total-cost-row">
                                 جمع کل: ۱۸۰۰۰ تومان
                             </div>
                         </div>
--- src/components/restaurant/restaurantContainer/cart/orderItem.js
+++ src/components/restaurant/restaurantContainer/cart/orderItem.js
@@ -1,25 +1,16 @@
 import React from 'react'
-<<<<<<< HEAD
 import './orderItem.css'
 import API from 'apis/api'
 import {eng2fa} from 'utils/utils'
-=======
-
-import API from 'apis/api'
-import './orderItem.css'
-
-
-const digitMap = {0:'۰', 1:'۱', 2:'۲', 3:'۳', 4:'۴', 5:'۵', 6:'۶', 7:'۷', 8:'۸', 9:'۹'}
-const eng2fa = (engNum) => {return((''+engNum).split('').map(digit=>digitMap[digit]).join(''))} 
->>>>>>> 70fb86f3aa933456fd913af84ad5421b19e2de60
 
 function OrderItem(props) {
 
     function handleClickPlus(e) {
         API.post('cart', {
-            restaurantId: `${props.item.food.restaurantId}`,
-            foodName: `${props.item.food.name}`            
+            restaurantId: `${props.item.restaurantId}`,
+            foodName: `${props.item.name}`            
         }).then(function (response) {
+            console.log(response);
             props.updateFunction();
           })
 
@@ -27,9 +18,10 @@ function OrderItem(props) {
 
     function handleClickMinus(e) {
         API.delete('cart', {data: {
-            restaurantId: `${props.item.food.restaurantId}`,
-            foodName: `${props.item.food.name}`            
+            restaurantId: `${props.food.restaurantId}`,
+            foodName: `${props.food.name}`            
         }}).then(function (response) {
+            console.log(response);
             props.updateFunction();
           })
     }
@@ -40,21 +32,21 @@ function OrderItem(props) {
     return(
         <div className="order-item-card">
             <div className="row mt-2 no-gutters">
-                <span className="col-auto mr-3 flaticon-minus minus-btn clickable no-gutters justify-content-start" onClick={handleClickMinus}></span>
-                <div className="col-auto flex-container no-gutters justify-content-center food-num-cart">
+                <span className="col-1 flaticon-minus minus-btn clickable no-gutters justify-content-start" onClick={handleClickMinus}></span>
+                <div className="col-2 flex-container no-gutters justify-content-center food-num-cart">
                     {eng2fa(props.item.number)}
                 </div>
-                <span className="col-auto ml-2 mr-3 flaticon-plus plus-btn clickable no-gutters justify-content-end" onClick={handleClickPlus}></span>
-                <div className="col-8 flex-container justify-content-end food-name-cart text-right">
+                <span className="col-1 flaticon-plus plus-btn clickable no-gutters justify-content-end" onClick={handleClickPlus}></span>
+                <div className="col-8 flex-container justify-content-end food-name-cart persian text-right">
                     {props.item.food.name}
                 </div>
             </div>
             <div className="row pt-0 pr-0 pb-2 pl-0 food-field-bottom container no-gutters">
-                <div className="col-auto ml-1 mr-1 pr-1 flex-container justify-content-end">تومان</div>
-                <div className="col-auto flex-container justify-content-start">
+                <div className="col-3 pr-1 flex-container justify-content-end">تومان</div>
+                <div className="col-3 flex-container justify-content-start">
                 {eng2fa(props.item.price)}
                 </div>
-                {/* <div className="col-6"></div> */}
+                <div className="col-6"></div>
             </div>
         </div>
     )
