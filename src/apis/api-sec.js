6f78b4b5cc5d3526eb200537c137d988524a2303
Hossein Entezari <h.entezari78@gmail.com>
2020-06-09 02:49:16 +0430

    backend link

--- src/apis/api-sec.js
+++ src/apis/api-sec.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://localhost:8080/'
+    // baseURL: 'https://localhost:8080/'
+    baseURL: 'https://http://185.166.105.6:31885/'
     // baseURL: 'http://3a536176.ngrok.io/'
 });
--- src/apis/api.js
+++ src/apis/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:8080/'
+    // baseURL: 'http://localhost:8080/'
+    baseURL: 'http://http://185.166.105.6:31885/'
 });
