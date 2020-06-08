6fb8d43f8026f5258efe77c2822326d173ad4766
Hossein Entezari <h.entezari78@gmail.com>
2020-06-09 03:06:21 +0430

    refactor addresses

--- src/apis/api-sec.js
+++ src/apis/api-sec.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default axios.create({
     // baseURL: 'https://localhost:8080/'
-    baseURL: 'https://http://185.166.105.6:31885/'
+    baseURL: 'https://185.166.105.6:31885/'
     // baseURL: 'http://3a536176.ngrok.io/'
 });
