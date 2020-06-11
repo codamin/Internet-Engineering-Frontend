325be78cb6a47c6795643a2b3b87c90a04160b9a
Hossein Entezari <h.entezari78@gmail.com>
2020-06-11 22:00:35 +0430

    address

--- src/apis/api.js
+++ src/apis/api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default axios.create({
     // baseURL: 'http://localhost:8080/'
-    // baseURL: 'http://185.166.105.6:31885/'
-    baseURL: 'http://ie.etuts.ir/31885'
+    baseURL: 'http://185.166.105.6:31885/'
+//     baseURL: 'http://ie.etuts.ir/31885'
 });
