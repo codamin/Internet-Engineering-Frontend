41500dd3643455147b5cec5375f1e230be50c094
Amin Asadi <aminasadi329@gmail.com>
2020-06-11 23:16:35 +0430

    clean apis

--- src/apis/api-sec.js
+++ src/apis/api-sec.js
@@ -1,8 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    // baseURL: 'https://localhost:8080/'
     baseURL: 'https://185.166.105.6:31885/'
-    // baseURL: 'https://ie.etuts.ir:31885'
-    // baseURL: 'http://3a536176.ngrok.io/'
 });
--- src/apis/api.js
+++ src/apis/api.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    // baseURL: 'http://localhost:8080/'
     baseURL: 'http://185.166.105.6:31885/'
-//     baseURL: 'http://ie.etuts.ir/31885'
 });
