62e41311b099835923fcdcf64e6bec53acb74a70
Hossein Entezari <h.entezari78@gmail.com>
2020-06-12 23:34:24 +0430

    deploy

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -97,11 +97,11 @@ class Home extends Component {
       <div>
         
 
-        {/* <Navbar cart={this.state.cart} updateFunction={this.updateCart} isHome={true} isProfile={false} /> */}
-        {/* <MainLogo updateRestaurants={this.updateRestaurants} /> */}
+        <Navbar cart={this.state.cart} updateFunction={this.updateCart} isHome={true} isProfile={false} />
+        <MainLogo updateRestaurants={this.updateRestaurants} />
         <FoodPartyMenu updateCart={this.updateCart}/>
-        {/* <RestaurantsMenu restaurants={this.state.restaurants} loadMore={this.loadMore}/> */}
-        {/* <Footer/> */}
+        <RestaurantsMenu restaurants={this.state.restaurants} loadMore={this.loadMore}/>
+        <Footer/>
       </div>
     );
   }
